b925971b2c0d08203632981f6cdb0261a9d1bffa
Alexander Wong <admin@alexander-wong.com>
2016-08-02 18:34:02 -0600

    React Fundamentals: Hello World - First Component

--- App.js
+++ App.js
@@ -2,10 +2,16 @@
 
 import React from 'react';
 
+/*
+// Stateful class method
 class App extends React.Component {
   render() {
-    return <div>Hi</div>;
+    return <h1>Hello World!</h1>
   }
 }
+*/
+
+// Stateless function component method
+const App = () => { return <h1>Hello Stateless World!</h1>};
 
 export default App;
