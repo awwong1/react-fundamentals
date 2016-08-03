7f9e96675f17118e07242831c0ab6679669a6d59
Alexander Wong <admin@alexander-wong.com>
2016-08-02 18:46:23 -0600

    React Fundamentals: Introduction to Properties

--- App.js
+++ App.js
@@ -1,19 +1,30 @@
 'use strict';
 
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
   render() {
-    // Render can only return one root node,
-    // parenthesis wrapping for whitespace
-    return (
-      <div>
-        <h1>Hello World!</h1>
-        <b>Bold</b>
-      </div>
-    );
+    // all React Components have props. These are attributes you pass to the component.
+    let txt = this.props.txt;
+    return <h1>{txt}</h1>
   }
 }
 
+// It is a good idea to define all React prop types that we use.
+App.propTypes = {
+  txt: React.PropTypes.string,
+  cat: React.PropTypes.number.isRequired
+};
 
-export default App;
+// You can also specify default properties of any React Component
+App.defaultProps = {
+  txt: 'This is the default text!',
+  cat: 0
+};
+
+ReactDOM.render(
+  //<App txt="this is the props value" cat={5} />,
+  <App cat={5} />,
+  document.getElementById('app')
+);
