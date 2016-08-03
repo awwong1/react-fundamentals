5ad5d9f9097d4f550a3b015394e2c2f51a573236
Alexander Wong <admin@alexander-wong.com>
2016-08-02 19:36:35 -0600

    React Fundamentals: Component Lifecycle - Mounting Basics

--- App.js
+++ App.js
@@ -1,24 +1,68 @@
 'use strict';
 
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      val: 0
+    };
+    this.update = this.update.bind(this);
+  }
+
+  update() {
+    this.setState({val: ( this.state.val || 0 ) + 1});
+  }
+
+  // Calls before the component mounts
+  componentWillMount() {
+    console.log('mounting');
+  }
+
+  // After component mounts, render once and whenever the dom changes
   render() {
-    // Can also include other components as well as text
-    return <Button>I <Heart /> React</Button>;
+    console.log('rendering');
+    return <button onClick={this.update}>{this.state.val}</button>
+  }
+
+  // Calls after the component mounts
+  componentDidMount() {
+    console.log('mounted');
+  }
+
+  // Calls before the component unmounts
+  componentWillUnmount() {
+    console.log('bye');
   }
 }
 
-class Button extends React.Component {
+class Wrapper extends React.Component {
+
+  constructor() {
+    super();
+    this.mount = this.mount.bind(this);
+    this.unmount = this.unmount.bind(this);
+  }
+
+  mount() {
+    ReactDOM.render(<App />, document.getElementById('a'));
+  }
+
+  unmount() {
+    ReactDOM.unmountComponentAtNode(document.getElementById('a'));
+  }
+
   render() {
-    // this.props.children directly references the values between the tags in App
-    return <button>{this.props.children}</button>;
+    return (
+      <div>
+        <button onClick={this.mount}>Mount</button>
+        <button onClick={this.unmount}>Unmount</button>
+        <div id="a"></div>
+      </div>
+    );
   }
 }
 
-const Heart = () => {
-  // Here we have a span with a bootstrap glyphicon heart
-  return <span className="glyphicon glyphicon-heart"></span>
-};
-
-export default App;
+export default Wrapper;
